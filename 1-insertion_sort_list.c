eb00131f21e8b610a431b621e9a0406294917d21
Payin14 <payin630@gmail.com>
2024-04-13 23:13:05 +0000

    1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,36 +2,61 @@
 
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
- *                       in ascending order using insertion sort.
+ *                       in ascending order using the Insertion Sort algorithm.
  * @list: Pointer to the head of the list to be sorted.
  */
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
+    if (!list || !(*list) || !(*list)->next)
         return;
 
-    listint_t *current = (*list)->next;
+    listint_t *current = *list;
 
-    while (current != NULL)
+    while (current->next)
     {
-        listint_t *temp = current->prev;
-        int key = current->n;
-
-        while (temp != NULL && temp->n > key)
+        if (current->n > current->next->n)
         {
-            temp->next->n = temp->n;
-            temp = temp->prev;
-        }
-
-        if (temp == NULL)
-        {
-            (*list)->n = key;
+            swap_nodes(current, &(current->next));
+            print_list(*list);
+            current = current->prev;
+            while (current->prev && current->prev->n > current->n)
+            {
+                if (!swap_nodes(current->prev, &(current)))
+                    break;
+                print_list(*list);
+            }
         }
         else
         {
-            temp->next->n = key;
+            current = current->next;
         }
-
-        current = current->next;
     }
+}
+
+/**
+ * swap_nodes - Swaps two nodes in a doubly linked list.
+ * @left: Pointer to the left node to swap.
+ * @right: Pointer to the right node to swap.
+ *
+ * Return: 1 on success, 0 on failure.
+ */
+int swap_nodes(listint_t *left, listint_t **right)
+{
+    if (!left || !(*right))
+        return 0;
+
+    listint_t *tmp = *right;
+    if (left->prev)
+        left->prev->next = tmp;
+    if (tmp->next)
+        tmp->next->prev = left;
+
+    left->next = tmp->next;
+    tmp->prev = left->prev;
+    left->prev = tmp;
+    tmp->next = left;
+
+    *right = tmp;
+
+    return 1;
 }
