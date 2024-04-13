c6a8c579f8653d5d9c90aba63335da0b055af6cc
Payin14 <payin630@gmail.com>
2024-04-13 22:42:03 +0000

    betty code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,11 +9,11 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    
     if (!array || size < 2)
         return;
+
+    size_t i, j;
+    int temp;
 
     for (i = 0; i < size - 1; i++)
     {
@@ -21,7 +21,6 @@ void bubble_sort(int *array, size_t size)
         {
             if (array[j] > array[j + 1])
             {
-               
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
