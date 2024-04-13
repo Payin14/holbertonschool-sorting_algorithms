51228a1589c87994b8d9771459b153200bcc0f1a
Payin14 <payin630@gmail.com>
2024-04-13 22:44:37 +0000

    a

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,11 +9,11 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    if (!array || size < 2)
-        return;
-
     size_t i, j;
     int temp;
+
+    if (!array || size < 2)
+        return;
 
     for (i = 0; i < size - 1; i++)
     {
