0ad6bb7fc625135b575a0a72c93ce608c185f281
Payin14 <payin630@gmail.com>
2024-04-13 23:46:24 +0000

    2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,32 +8,28 @@
   */
 void selection_sort(int *array, size_t size)
 {
-    size_t i = 0, j = 1, temp = 0, min_index = 0, limit = size - 1;
+	size_t i = 0, j = 1, temp = 0, min_index = 0, limit = size - 1;
 
-    if (size < 2)                    
-        return;
-
-    while (i < limit)
-    {
-        if (j == size)               
-        {
-            if (i != min_index)      
-            {
-                temp = array[i];     
-                array[i] = array[min_index];
-                array[min_index] = temp;
-                print_array(array, size); 
-            }
-
-            ++i;                    
-            min_index = i;          
-            j = i + 1;              
-            continue;               
-        }
-
-        if (array[min_index] > array[j]) 
-            min_index = j;               
-
-        ++j;                 
-    }
+	if (size < 2)
+		return;
+	while (i < limit)
+	{
+		if (j == size)
+		{
+			if (i != min_index)
+			{
+				temp = array[i];
+				array[i] = array[min_index];
+				array[min_index] = temp;
+				print_array(array, size);
+			}
+			++i;
+			min_index = i;
+			j = i + 1;
+			continue;
+		}
+		if (array[min_index] > array[j])
+			min_index = j;
+		++j;
+	}
 }
